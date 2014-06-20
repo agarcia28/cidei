d5d12d3547c97708f4f2f78a8c3cd0d79530ae31
Ana Maria Garcia <agarcia@cidei.net>
2014-06-20 18:10:18 -0500

    Comienza backend

---
+++ Frontend/lab9/lab9-2/dist/js/650f31ed.lab9-2.js
@@ -0,0 +1,33 @@
+// Comentario de linea sencilla
+/*
+Comentario
+multi-linea
+*/
+
+/**
+* @author Ana Maria Garcia agarcia@cidei.net
+* @license MIT
+* @copyright Todos los derechos reservados
+*/
+$(document).ready(function(){
+	$('#getInfo').click(function(e){
+		var data = {};
+		data.title = "hola";
+		data.message = "message";
+
+		$.ajax({
+			type : 'POST',
+			data : JSON.stringify(data),
+			contentType : 'application/json',
+			url : 'http://127.0.0.1:8585/values',
+			success : function(data){
+				console.log('success');
+				//data = JSON.stringify(data);
+
+				$.each(data, function(){
+					$('#info').append('<span>' + value + '</span');
+				});
+			}
+		});
+	});
+});
